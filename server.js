f4ddbcf352110301b542f3bdfe5bb04f7209689b
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2021-04-29 14:01:23 +0000

    app & docker

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const user = data.find((item) => item.id === id);
+  res.status(200).send(user);
+});
+
+app.listen(3000, () => console.log("server run"));
